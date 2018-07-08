6c5bbc151f9aa7e6a5ea18f3f9cb2a1e0f12b7fe
hanson1218 <you@example.com>
2018-07-08 15:14:31 +0800

    add log4j

--- src/main/java/com/snow/www/controller/UserController.java
+++ src/main/java/com/snow/www/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.snow.www.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,15 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.snow.www.service.UserService;
 
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
+	
+	private static Logger Logger = LoggerFactory.getLogger(UserController.class);
 
 	@RequestMapping("/{id}")
 	public String getNameById(@PathVariable int id){
+		try {
+			int a = 1/0;
+		} catch (Exception e) {
+			Logger.error("test");
+		}
+		
 		return userService.getNameById(id);
 	}
 }
--- src/main/java/com/snow/www/service/impl/UserServiceImpl.java
+++ src/main/java/com/snow/www/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.snow.www.dao.UserMapper;
+import com.snow.www.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
